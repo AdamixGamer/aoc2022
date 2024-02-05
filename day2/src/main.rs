ab9c2ac3872f54acc60c9580fa7cb4035d73cb02
a a <smoczymistrz2019@gmail.com>
2024-02-05 21:09:54 +0000

    day 2 full complete

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -2,6 +2,10 @@ fn main() {
     let input = include_str!("input");
     let result:u32 = input.lines().map(action).sum();
     println!("{result}");
+
+    let input = include_str!("input");
+    let result:u32 = input.lines().map(actionresult).sum();
+    println!("{result}");
 }
 
 fn action(input: &str) -> u32{
@@ -15,6 +19,21 @@ fn action(input: &str) -> u32{
         "C X" => 7,
         "C Y" => 2,
         "C Z" => 6,
+        _ => panic!()
+        }
+
+}
+fn actionresult(input: &str) -> u32{
+    match input {
+        "A X" => 3,
+        "A Y" => 4,
+        "A Z" => 8,
+        "B X" => 1,
+        "B Y" => 5,
+        "B Z" => 9,
+        "C X" => 2,
+        "C Y" => 6,
+        "C Z" => 7,
         _ => panic!()
         }
 
